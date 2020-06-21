a6e774d1eb957757bb4a6db46f6b1a8a8fb91232
Rakshit Malhotra <39698255+Raks-coder@users.noreply.github.com>
2020-06-21 16:14:18 +0530

    begin

---
+++ begin.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class begin : MonoBehaviour
+{
+    public void beginGame()
+    {
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
