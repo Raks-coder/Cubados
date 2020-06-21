7fea6b7013ac58a65f3335fc62deddc232d618eb
Rakshit Malhotra <39698255+Raks-coder@users.noreply.github.com>
2020-06-21 16:13:58 +0530

    Game-Manager_script

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	bool flag=false;
+	public float delay=2f;
+   public GameObject clUI;
+   public void completelevel()
+   {
+      clUI.SetActive(true);
+   }
+   public void GameOver()
+   {
+   	if(flag==false)
+   	{
+   	flag=true;
+   	Debug.Log("Game Over!!!");
+   	Invoke("Restart", delay);
+   }
+   }
+   void Restart ()
+   {
+   	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+   }
+}
