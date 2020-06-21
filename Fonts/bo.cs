d2a413965ec75a77492d49675514dd46a62d21b5
Rakshit Malhotra <39698255+Raks-coder@users.noreply.github.com>
2020-06-21 16:12:12 +0530

    Fonts

---
+++ Fonts/bo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class bo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Text textscore;
+    // Update is called once per frame
+     void Update()
+    {
+        textscore.text=player.position.z.ToString("0");
+    }
+}
